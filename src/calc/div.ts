fea56bcf40b5ad4b2f1a1b76919f464ae2718c49
Lucas Avelino Fraga <157542216+lucasfragadev@users.noreply.github.com>
2024-12-03 19:44:36 -0300

    Update div.ts

--- src/calc/div.ts
+++ src/calc/div.ts
@@ -1,1 +1,8 @@
-
+export default function div (a: number, b: number): number | undefined {
+  if (b === 0) {
+    console.log('Divisão por zero.');
+    return undefined;
+  } else {
+    return a / b;
+  }
+};
