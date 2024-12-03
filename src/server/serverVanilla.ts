edc99d192bbc187a6cd468b08c0b2993607abb8b
Lucas Avelino Fraga <157542216+lucasfragadev@users.noreply.github.com>
2024-12-03 19:44:07 -0300

    Update serverVanilla.ts

--- src/server/serverVanilla.ts
+++ src/server/serverVanilla.ts
@@ -1,1 +1,17 @@
+import * as http from 'http';
+
+const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+  if (req.url === '/' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World! Welcome to my life!');
+  } else {
+    res.writeHead(400, { 'Content-Type': 'text/plain' });
+    res.end('Erro na solicitação');
+  }
+});
+
+server.listen(8080, () => {
+  console.log('Servidor rodando na porta 8080');
+  console.log('http://localhost:8080/');
+});
 
