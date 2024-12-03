067677a87aa03c14f6171e7ce24a6622594a7165
Lucas Avelino Fraga <157542216+lucasfragadev@users.noreply.github.com>
2024-12-03 19:44:21 -0300

    Update calc.ts

--- src/calc/calc.ts
+++ src/calc/calc.ts
@@ -1,1 +1,10 @@
+import soma from './soma'
+import div from './div'
+import mult from './mult'
+import sub from './sub'
+
+console.log(soma(5, 5));
+console.log(div(2, 0));
+console.log(mult(2, 5));
+console.log(sub(2, 5));
 
